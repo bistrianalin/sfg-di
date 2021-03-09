2faa61f1abcb4b9b33ccd295cf75d5ee30cb91a3
Alin <alin.bistrian@sky.uk>
2021-03-09 13:47:55 +0000

    39 Default Profiles

--- src/main/java/guru/springframework/sfgdi/controllers/SetterInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/SetterInjectedController.java
@@ -1,6 +1,5 @@
 package guru.springframework.sfgdi.controllers;
 
-import guru.springframework.sfgdi.services.ConstructorGreetingService;
 import guru.springframework.sfgdi.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
